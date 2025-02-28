d594aac4ed6cb4be6db0fbc56621c79c4230cd09
Yulian Khai <yulik19971997@gmail.com>
2025-02-28 18:52:01 +0200

    Redux Store. Api Slice

---
+++ frontend/src/constants.tsx
@@ -0,0 +1,14 @@
+
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+const PRODUCT_URL = '/api/products';
+const USERS_URL = '/api/users';
+const ORDERS_URL = '/api/orders';
+const PAYPAL_URL = '/api/config/paypal';
+
+export {
+	BASE_URL,
+	PRODUCT_URL,
+	USERS_URL,
+	ORDERS_URL,
+	PAYPAL_URL,
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store.tsx'
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -19,6 +21,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+	<Provider store={store}>
+    	<RouterProvider router={router} />
+	</Provider>
   </StrictMode>,
 )
