1dd78bc5ed1de74ec85268339fbfe8517977638a
Yulian Khai <yulik19971997@gmail.com>
2025-03-01 14:13:53 +0200

    Cart Item Count in Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import {Navbar, Nav, Container, Badge} from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store';
 
 export const Header = () => {
+	const { cartItems } = useSelector((state: RootState) => state.cart)
+
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -15,7 +19,14 @@ export const Header = () => {
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
-							<Link to='/cart' className='nav-link'><FaShoppingCart /> Cart</Link>
+							<Link to='/cart' className='nav-link'>
+								<FaShoppingCart /> Cart
+								{cartItems.length > 0 && (
+									<Badge pill bg='success' style={{marginLeft: '5px'}}>
+										{cartItems.reduce((acc, item) => acc + +item.qty, 0)}
+									</Badge>
+								)}
+							</Link>
 							<Link to='/login' className='nav-link'><FaUser /> Sign In</Link>
 						</Nav>
 					</Navbar.Collapse>
