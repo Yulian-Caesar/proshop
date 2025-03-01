8d08137ed3532ff02eb7023b79faaec7a9c7745b
Yulian Khai <yulik19971997@gmail.com>
2025-03-01 23:07:30 +0200

    User register Endpoint & Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// Hash our password before save to DB
+userSchema.pre('save', async function (next) {
+	if(!this.isModified('password')) {
+		return next()
+	}
+
+    const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt)
+
+	next()
+})
+
 const User = mongoose.model("User", userSchema)
 
 export default User;
