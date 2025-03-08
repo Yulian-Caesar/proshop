8f7f53e97c6c5ce42d4b3e371541ea44dc96c952
Yulian Khai <yulik19971997@gmail.com>
2025-03-08 22:20:09 +0200

    Fix bug

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,6 @@ app.use(express.urlencoded({ extended: true }))
 // Cookie parser middleware
 app.use(cookieParser())
 
-
-app.get('/', (req, res) => {
-	res.send('API is running...');
-})
-
 app.use('/api/products', ProductRoutes)
 app.use('/api/users', UserRoutes)
 app.use('/api/orders', OrderRoutes)
