22daffba356a57534e6e662b60028ddf887ce847
Yulian Khai <yulik19971997@gmail.com>
2025-03-08 14:25:01 +0200

    Delete User

--- frontend/src/pages/admin/UserList.tsx
+++ frontend/src/pages/admin/UserList.tsx
@@ -1,22 +1,37 @@
 import Loader from "../../components/Loader/Loader"
 import { Message } from "../../components/Message/Message"
-import { useGetUsersQuery } from "../../slices/usersApiSlice"
+import { useDeleteUserMutation, useGetUsersQuery } from "../../slices/usersApiSlice"
 import UserTable from "../../components/UserTable/UserTable"
+import { toast } from "react-toastify"
 
 export const UserList = () => {
 	const {data: users, refetch, isLoading, error} = useGetUsersQuery()
+	const [deleteUser, { isLoading: loadingDelete }] = useDeleteUserMutation()
 
-	const deleteHandler = async(id: string) => {
-		console.log('delete')
+	const deleteHandler = async(userId: string) => {
+		if(window.confirm("Are you sure you want to delete user?")) {
+			try {
+				await deleteUser(userId)
+				toast.success("User deleted")
+				refetch()
+			} catch (err) {
+				toast.error(err?.data?.message || err?.message)
+			}
+		}
 	}
-
-	if(isLoading) return <Loader />
-	if(error) return <Message variant="danger">{error}</Message>
 
 	return (
 		<>
 			<h1>Users List</h1>
-			<UserTable users={users} deleteHandler={deleteHandler} />
+			{loadingDelete && <Loader />}
+
+			{isLoading ? (
+				<Loader />
+			) : error ? (
+				<Message variant="danger">{error}</Message>
+			) : (
+				<UserTable users={users} deleteHandler={deleteHandler} />
+			)}
 		</>
 	)
 }
